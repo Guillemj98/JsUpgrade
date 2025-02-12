e0ee44b709a1eb02dd8a7df6c806c825e7d8ef5c
Guillemj98 <guillemagro98@gmail.com>
2025-02-12 16:10:34 +0100

    Estructura basica del modal

--- ReactUpgrade/reacTest/src/component/Modal.jsx
+++ ReactUpgrade/reacTest/src/component/Modal.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import "../styles/Modal.css"
 
 
-export default function Modal() {
+export default function Modal(props) {
 
    return (
+      <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-icon">
+          <span>ℹ️</span>
+        </div>
+        <p className="modal-text">Save changes ?</p>
+        <div className="modal-buttons">
+          <button onClick={props} className="discard-button">Discard</button>
+          <button onClick={props} className="save-button">Save</button>
+        </div>
+      </div>
+    </div>
      
    )
 

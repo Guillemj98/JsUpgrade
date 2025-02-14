cf743dba0591e2d3014c754d5742264489a13fad
Guillemj98 <guillemagro98@gmail.com>
2025-02-14 15:43:03 +0100

    cambios

--- ReactUpgrade/reacTest/src/App.jsx
+++ ReactUpgrade/reacTest/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Note from './pages/Note';
 import ImagePlaceHolder from './component/imagePlaceHolder';
 import "./App.css"
+import Modal from './component/Modal';
 
 
 export default function App() {
@@ -11,6 +12,10 @@ export default function App() {
       <div className='app'>
 
          <Note/>
+         <Modal mensaje='¿Guardar cambios?'/>
+         <Modal mensaje='¿Seguro que quieres descartar los cambios?'/>
+         
+       
 
       </div>
    )
--- ReactUpgrade/reacTest/src/component/Modal.jsx
+++ ReactUpgrade/reacTest/src/component/Modal.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import "../styles/Modal.css"
+import { IoMdInformationCircle } from "react-icons/io";
 
 
 export default function Modal(props) {
 
    return (
-<<<<<<< HEAD
-      <div className='imagePlaceholder'>
-      <img className='imagePlaceholder_image' src="/Users/charlygoomezz/Desktop/ReactDI/reactDI/src/assets/a0c6e54564983ed54d21877a26ecceb1.jpg" alt="Imagen" />
-      <p className='imagePlaceholder_text'></p>
-  </div>
-=======
-      <div className="modal-overlay">
-      <div className="modal-container">
-        <div className="modal-icon">
-          <span>ℹ️</span>
+
+      <div>
+      <div className="modal">
+        <div className="modal__icon">
+          <span><IoMdInformationCircle/></span>
         </div>
-        <p className="modal-text">Save changes ?</p>
-        <div className="modal-buttons">
-          <button onClick={props} className="discard-button">Discard</button>
-          <button onClick={props} className="save-button">Save</button>
+        <p className="modal__text">{props.mensaje}</p>
+        <div className="modal__buttons">
+          <button onClick={props.discard} className="modal__button modal__button--red">Discard</button>
+          <button onClick={props.save} className="modal__button modal__button--green ">Save</button>
         </div>
       </div>
     </div>
->>>>>>> e0ee44b709a1eb02dd8a7df6c806c825e7d8ef5c
+
      
    )
 
